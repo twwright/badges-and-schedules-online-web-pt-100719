3d3350f69266ef202921d335d72a2889fb1313b6
Thomas Wright <twwright@gmail.com>
2019-10-19 19:12:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,16 @@
-badge_maker(name)
+def badge_maker(name)
   "Hello, #{name}!"
 end
 
-batch_badge_maker(names)
+def batch_badge_maker(names)
   names.collect {|name| badger_maker(name)}
 end
 
-assign_rooms(rooms)
+def assign_rooms(rooms)
   rooms.collect.with_index(1) {|person, idx| "Hello, #{person}! You'll be assigned to room #{idx}!"}
 end
 
-printer(array)
+def printer(array)
   batch_badge_maker(array).each {|str| puts str}
   assign_rooms(array).each {|str| puts str}
 end
