890f61527a227e7e5c159ac607a4077eff213375
Thomas Wright <twwright@gmail.com>
2019-10-19 19:05:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,16 @@
 badge_maker(name)
-  puts "Hello #{name}"
+  "Hello, #{name}!"
 end
+
+batch_badge_maker(names)
+  names.collect {|name| badger_maker(name)}
+end
+
+assign_rooms(rooms)
+  rooms.collect_with_index {|person, idx| "Hello, #{person}! You'll be assigned to room #{idx + 1}!"}
+end
+
+printer(array)
+  assignments = []
+  assignments.collect {|}
+  
